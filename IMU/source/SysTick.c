618b7f3a224ddc39a2898b13f323184883c5e9fb
tlifschitz <tlifschitz@itba.edu.ar>
2018-09-21 18:58:56 -0300

    Agregue systick para mandarme cosas solo

---
+++ IMU/source/SysTick.c
@@ -0,0 +1,132 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                              CONTROL DE ACCESO                              //
+//          Grupo 3 - Laboratorio de Microprocesadores - ITBA - 2018           //
+//	                                                                           //
+/////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file SysTick.c
+ *
+ */
+
+
+#include "SysTick.h"
+#include "Board.h"
+
+
+// If defined, a GPIO pin is raised during sysTickHandler execution, in order
+// to make measurements.
+//#define DEBUG_SYSTICK
+
+#ifdef DEBUG_SYSTICK
+#include "GPIO.h"
+#endif
+
+#define SYSTICK_TEST_PIN PORTNUM2PIN(PD,1)
+
+
+// Structure to store SysTick callbacks
+typedef struct{
+	SysTickFnc f;        // Function pointer
+	uint32_t resetValue; // Reset value for the counter
+	uint32_t count;      // Counter
+}SysTickCallback;
+
+// Static array to store callbacks
+static SysTickCallback callbacks[SYSTICK_MAX_CALLBACKS];
+static int callbacksSize;
+
+static uint64_t _millisCount;
+
+static void millisCount()
+{
+	_millisCount++;
+}
+
+uint64_t millis()
+{
+	return _millisCount;
+}
+
+
+uint32_t sysTickInit()
+{
+#ifdef DEBUG_SYSTICK
+		pinMode(SYSTICK_TEST_PIN,OUTPUT);
+#endif
+
+	uint32_t ticks = SYSTICK_ISR_PERIOD_S*__CORE_CLOCK__;
+
+	if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk)
+	    return (0UL);
+
+	SysTick->CTRL = 0x00;  // Disable the timer
+	SysTick->LOAD = ticks -1;
+	SysTick->VAL  = 0UL;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk|SysTick_CTRL_TICKINT_Msk|SysTick_CTRL_ENABLE_Msk;
+
+	sysTickAddCallback(&millisCount,1*MS2S);
+	return (1UL);
+}
+
+bool sysTickAddCallback(SysTickFnc fnc,float period)
+{
+	// If the array is not full
+	if(callbacksSize < SYSTICK_MAX_CALLBACKS)
+	{
+		callbacks[callbacksSize].f = fnc;
+		callbacks[callbacksSize].resetValue = (uint32_t)(period/SYSTICK_ISR_PERIOD_S+0.5);
+		callbacks[callbacksSize].count = callbacks[callbacksSize].resetValue;
+		callbacksSize++;
+		return true;
+	}
+	else return false;
+}
+
+bool sysTickAddDelayCall(SysTickFnc fnc,float time)
+{
+	// If the array is not full
+	if(callbacksSize < SYSTICK_MAX_CALLBACKS)
+	{
+		callbacks[callbacksSize].f = fnc;
+		callbacks[callbacksSize].resetValue = 0; // The reset counter is cero, call only once!
+		callbacks[callbacksSize].count = time/SYSTICK_ISR_PERIOD_S;
+		callbacksSize++;
+		return true;
+	}
+	else return false;
+}
+
+
+__ISR__ SysTick_Handler(void)
+{
+#ifdef DEBUG_SYSTICK
+		digitalWrite(SYSTICK_TEST_PIN,1);
+#endif
+	// Iterate all registered functions
+	for(int i=0;i<callbacksSize;i++)
+	{
+		callbacks[i].count--;
+
+		if(callbacks[i].count==0)
+		{
+			// If timeouts, execute
+			(*callbacks[i].f)();
+			// And either reset the counter, or unregister
+			if(callbacks[i].resetValue!=0)
+				callbacks[i].count = callbacks[i].resetValue;
+			else
+			{
+				for(int j=i+1;j<callbacksSize;j++)
+				{
+					callbacks[j-1]=callbacks[j];
+				}
+				callbacksSize--;
+				i--;
+			}
+		}
+	}
+#ifdef DEBUG_SYSTICK
+		digitalWrite(SYSTICK_TEST_PIN,0);
+#endif
+}
---
+++ IMU/source/SysTick.h
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////////////////////
+//                              CONTROL DE ACCESO                              //
+//          Grupo 3 - Laboratorio de Microprocesadores - ITBA - 2018           //
+//	                                                                           //
+/////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file Systick.h
+ * @brief Systick driver to register periodic callbacks calls or single calls.
+ */
+
+#ifndef _SYSTICK_H_
+#define _SYSTICK_H_
+
+/////////////////////////////////////////////////////////////////////////////////
+//                             Included header files                           //
+/////////////////////////////////////////////////////////////////////////////////
+#include <stdint.h>
+#include <stdbool.h>
+#include "hardware.h"
+
+/////////////////////////////////////////////////////////////////////////////////
+//                       Constants and macro definitions                       //
+/////////////////////////////////////////////////////////////////////////////////
+
+#define SYSTICK_MAX_CALLBACKS 10
+#define MS2S (1/1000.0)
+#define US2S (1/1000000.0)
+#define SYSTICK_ISR_PERIOD_S (1*MS2S)
+
+/////////////////////////////////////////////////////////////////////////////////
+//                    Enumerations, structures and typedefs                    //
+/////////////////////////////////////////////////////////////////////////////////
+typedef void(*SysTickFnc)(void);
+
+/////////////////////////////////////////////////////////////////////////////////
+//                         Global function prototypes                          //
+/////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Init systick driver
+ * @return Init succeed
+ */
+uint32_t sysTickInit();
+
+/**
+ * @brief Register a periodic call to a function.
+ * @param period Period of the function call.
+ * @return Register succeed. If false, max. number of callbacks reached.
+ */
+bool sysTickAddCallback(SysTickFnc fnc,float period);
+
+/**
+ * @brief Register a single call to a function, after a certain delay.
+ * @param time Time to wait before the function call.
+ * @return Register succeed. If false, max. number of callbacks reached.
+ */
+bool sysTickAddDelayCall(SysTickFnc fnc,float time);
+
+/**
+ * @brief Return the number of milliseconds since the program execution started.
+ * @return Time in milliseconds.
+ */
+uint64_t millis();
+
+
+/////////////////////////////////////////////////////////////////////////////////
+#endif // _SYSTICK_H_
