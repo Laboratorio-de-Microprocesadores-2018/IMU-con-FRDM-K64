1184b60127f64b17e660994632cfb8d01412136c
tlifschitz <tlifschitz@itba.edu.ar>
2018-09-26 12:17:09 -0300

    Agregue el main de App

--- IMU/source/App.c
+++ IMU/source/App.c
@@ -9,23 +9,21 @@
  * @brief
  */
 
-#include "UART.h"
-#include "GPIO.h"
-#include "FXOS8700CQDriver.h"
-
 /////////////////////////////////////////////////////////////////////////////////
 //                             Included header files                           //
 /////////////////////////////////////////////////////////////////////////////////
+#include "FXOS8700CQDriver.h"
 #include "CANCommunications.h"
+#include "UART.h" // REEMPLAZAR POR LA CAPA DE ARRIBA
 #include "SysTick.h"
 #include "math.h"
 
 /////////////////////////////////////////////////////////////////////////////////
 //                       Constants and macro definitions                       //
 /////////////////////////////////////////////////////////////////////////////////
-#define MEASURE_PERIOD_MS 100 // Time in milliseconds between two position measurements
-#define TIMEOUT_MS 2000
-#define THRESHOLD 5
+#define MEASURE_PERIOD_MS 100 // Time in milliseconds between two measurements
+#define TIMEOUT_MS 2000		// Maximum time in milliseconds between to measurements
+#define THRESHOLD 5 		// Threshold in degrees for measurements
 
 /////////////////////////////////////////////////////////////////////////////////
 //                    Enumerations, structures and typedefs                    //
@@ -35,13 +33,7 @@ typedef struct{
 	int roll;
 	int pitch;
 	int yaw;
-}AngularPosition;
-
-typedef struct{
-	struct Vector{
-		uint32_t x,y,z;
-	}accelerometer,magnetometer;
-}RawData;
+}Orientation;
 
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -52,15 +44,16 @@ typedef struct{
 //                   Local variable definitions ('static')                     //
 /////////////////////////////////////////////////////////////////////////////////
 
+static sData accelerometer,magnetometer;
 static uint32_t lastMeasureTime,lastRollTime,lastPitchTime;
-static AngularPosition lastPos;
+static Orientation lastPos;
 static Measurement m;
 
 /////////////////////////////////////////////////////////////////////////////////
 //                   Local function prototypes ('static')                      //
 /////////////////////////////////////////////////////////////////////////////////
 
-static AngularPosition computePosition(RawData d);
+static Orientation computePosition(sData accelerometer,sData magnetometer);
 
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -74,39 +67,39 @@ void App_Init (void)
 	FX_config accelConfig = FX_GetDefaultConfig();
 	FX_Init(accelConfig);
 
-	pinMode(PIN_LED_RED,OUTPUT);
-	digitalWrite(PIN_LED_RED,1);
 
 }
-static int i=0;
+
 
 void App_Run (void)
 {
-
 	uint64_t now = millis();
 
 	if((now-lastMeasureTime)>MEASURE_PERIOD_MS)
 	{
-		RawData d ;//= accelometerGetRawData();
-		AngularPosition currPos = computePosition(d);
-		if(fabs(currPos.roll-lastPos.roll)>THRESHOLD || (now-lastRollTime) > TIMEOUT_MS)
+		if(FX_GetData(&accelerometer,&magnetometer)==true)
 		{
-			m.boardID = MY_BOARD_ID;
-			m.angleID = 'R';
-			m.angleVal = currPos.roll;
-			sendMeasurement2OtherBoards(m); // Si habilito self-receive en CAN se manda solo a la compu mas abajo
-			lastRollTime = now;
+			Orientation currPos = computePosition(accelerometer,magnetometer);
 
+			if(fabs(currPos.roll-lastPos.roll)>THRESHOLD || (now-lastRollTime) > TIMEOUT_MS)
+			{
+				m.boardID = MY_BOARD_ID;
+				m.angleID = 'R';
+				m.angleVal = currPos.roll;
+				sendMeasurement2OtherBoards(m); // Si habilito self-receive en CAN se manda solo a la compu mas abajo
+				lastRollTime = now;
+			}
+
+			if(fabs(currPos.pitch-lastPos.pitch)>THRESHOLD || (now-lastPitchTime) > TIMEOUT_MS)
+			{
+				m.boardID = MY_BOARD_ID;
+				m.angleID = 'P';
+				m.angleVal = currPos.roll;
+				sendMeasurement2OtherBoards(m);
+				lastPitchTime = now;
+			}
+			lastMeasureTime = now;
 		}
-		if(fabs(currPos.pitch-lastPos.pitch)>THRESHOLD || (now-lastPitchTime) > TIMEOUT_MS)
-		{
-			m.boardID = MY_BOARD_ID;
-			m.angleID = 'P';
-			m.angleVal = currPos.roll;
-			sendMeasurement2OtherBoards(m);
-			lastPitchTime = now;
-		}
-		lastMeasureTime = now;
 	}
 
 	if(receiveOtherBoardsMeasurement(&m) == true);
@@ -115,8 +108,12 @@ void App_Run (void)
 
 }
 
-AngularPosition computePosition(RawData d)
+Orientation computePosition(sData accelerometer,sData magnetometer)
 {
-
-
+	Orientation o;
+	// CUENTAS
+	o.pitch=0;
+	o.roll=0;
+	o.yaw=0;
+	return o;
 }
